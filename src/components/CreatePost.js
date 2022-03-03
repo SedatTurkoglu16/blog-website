3344371ac81f7f892ffcfee16215b2257b5d7eaa
SedatTurkoglu16 <sedatturkoglu16@gmail.com>
2022-03-03 13:17:48 +0300

    Retrieving and displaying posts, done.

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -20,7 +20,6 @@ const CreatePost = ({ isAuth }) => {
     navigate("/");
   };
 
-  
   useEffect(() => {
     if (!isAuth) {
       navigate("/login");
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { getDocs, collection } from 'firebase/firestore'
+import { db } from '../firebase-config'
+import './Home.css'
 
 const Home = () => {
+  const [postLists, setPostLists] = useState([]);
+  const postsCollectionRef = collection(db, "posts");
+
+  useEffect(() => {
+    const getPosts = async () => {
+      const data = await getDocs(postsCollectionRef);
+      setPostLists(data.docs.map((doc) => ({ ...doc.data(), id: doc.id})));
+    }
+    getPosts();
+  })
+
   return (
-    <div>Home</div>
+    <div className='homePage'>
+      {postLists.map((post) => {
+        return (
+        <div className='post'>
+          <div className='postHeader'>
+            <div className='title'>
+              <h1>{post.title}</h1>
+            </div>
+          </div>
+          <div className='postTextContainer'> {post.postText} </div>
+          <h5 className='authorName'>@{post.author.name}</h5>
+        </div>
+        )
+      })
+
+      }
+    </div>
   )
 }
 
